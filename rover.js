2b403a0d28873e5271db82ad60414fb19bdba232
Dominique Gould <dominiquegould42@yahoo.com>
2024-02-26 19:50:58 -0600

    1 Rover test passed

--- rover.js
+++ rover.js
@@ -1,6 +1,12 @@
 class Rover {
-   constructor(position,
-   // Write code here!
+   constructor(position, mode = 'NORMAL', generatorWatts = 110) {
+      this.position = position;
+      if (!position) {
+        throw Error("Position required.");
+      }
+      this.mode = mode;
+      this.generatorWatts = generatorWatts;
+   }
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,63 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
+let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+let message = new Message('Test message with two commands', commands);
+let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+// let response = rover.receiveMessage(message);
+
+// console.log(response);
   // 7 tests here!
+
+  // test 7
+it("constructor sets position and default values for mode and generatorWatts", function() {
+  let testRover = new Rover(98385);
+    
+  expect(testRover.position).toBe(98385);
+  expect(testRover.mode).toBe('NORMAL');
+  expect(testRover.generatorWatts).toBe(110);
+});
+
+
+// test 8
+// it("response returned by receiveMessage contains the name of the message", function() {
+//   let testRoverTwo = new Rover();
+    
+//   expect(testRoverTwo.).toBe();
+// });
+
+
+// test 9
+// it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+//   let testRoverThree = new Rover();
+    
+//   expect(testRoverThree.).toBe();
+// });
+
+
+// test 10
+// it("responds correctly to the status check command", function() {
+//   let testRoverFour = new Rover();
+    
+//   expect(testRoverFour.).toBe();
+// });
+
+
+// test 11
+// it("responds correctly to the mode change command", function() {
+//   let testRoverFive = new Rover();
+    
+//   expect(testRoverFive.).toBe();
+// });
+
+
+// test 12
+// it("responds with a false completed value when attempting to move in LOW_POWER mode", function() {
+//   let testRoverSix = new Rover();
+    
+//   expect(testRoverSix.).toBe();
+// });
+
+
 
 });
